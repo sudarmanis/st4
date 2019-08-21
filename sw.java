3edcc0cf58ba7f02e27e883e1cfbfd1264358217
sudarmanis <41697795+sudarmanis@users.noreply.github.com>
2019-08-21 06:04:15 -0700

    Create sw.java

---
+++ sw.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class sw
+{
+public static void main(String argS[])
+{
+	int t=0;
+
+String s;
+Scanner sc=new Scanner(System.in);
+s=sc.nextLine();
+String a[]=s.split(" ");
+for (int i=0;i<a.length;i++)
+{
+	t++;
+}System.out.println(t);
+}}
