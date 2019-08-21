17c8b484732d05bf165c5a9c53acdc46145bce4b
sudarmanis <41697795+sudarmanis@users.noreply.github.com>
2019-08-21 06:24:09 -0700

    Create sa.java

---
+++ sa.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class sa
+{
+public static void main(String argS[])
+{
+	int t=0;
+
+String s;
+Scanner sc=new Scanner(System.in);
+s=sc.nextLine();
+String a[]=s.split(" ");
+for (int i=1;i<a.length;i++)
+{t++;
+	
+}System.out.println(t);
+}}
