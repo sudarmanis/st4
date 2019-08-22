b90353839322b64717f7c5f90c2f354ae06e28da
sudarmanis <41697795+sudarmanis@users.noreply.github.com>
2019-08-22 07:43:17 -0700

    Create no.java

---
+++ no.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class no
+{
+public static void main(String argS[])
+{
+int a,b;
+Scanner sc=new Scanner(System.in);
+a=sc.nextInt();
+b=sc.nextInt();
+a=a+b;
+b=a-b;
+a=a-b;
+System.out.println(a+" "+b);
+}
+}
